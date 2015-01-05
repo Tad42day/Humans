b02c4c7676bf20d63548f63deb6ed086bd3f64c7
Talisson Bento <talisson.dev@gmail.com>
2015-01-05 20:12:27 -0200

    Commit initial

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float VelocityWalk;
+	public float VelocityFly;
+
+
+	public float angles;
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.eulerAngles = new Vector3(0, 0, (angles));
+		angles = (angles + 0.1f) % 360;
+	}
+}
---
+++ Assets/Sprite/Misc/Cursor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+	private static Vector2 Position;
+	private float angle = 0f;
+
+
+	private float VelRotation = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		Position = new Vector2(10, 10);
+	}
+
+	public static float x
+	{
+		get
+		{
+			return Position.x;
+		}
+
+	}
+
+	public static float y
+	{
+		get
+		{
+			return Position.y;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Position = new Vector2 (Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+			
+		angle = (angle + VelRotation) % 360;
+	}
+}
